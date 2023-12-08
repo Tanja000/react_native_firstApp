be24a77038a41705fa298ae6ed5d7c6512f92ad2
Tanja <tanjareize@web.de>
2023-12-08 20:55:51 +0100

    icons for tabs

--- App.js
+++ App.js
@@ -1,22 +1,59 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Expenses from './screens/Expenses';
 import New from './screens/New';
 import Report from './screens/Report';
 import Settings from './screens/Settings';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => (
   <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Expenses" component={Expenses} />
-      <Tab.Screen name="Report" component={Report} />
-      <Tab.Screen name="New" component={New} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen 
+        name="Expenses" 
+        component={Expenses} 
+        options={{
+          tabBarLabel: 'Expenses',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="currency-eur" color={color} size={26} />
+          ),
+        }}  
+        />
+      <Tab.Screen 
+        name="Report" 
+        component={Report} 
+        options={{
+          tabBarLabel: 'Report',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="chart-bar" color={color} size={26} />
+          ),
+        }}    
+        />
+      <Tab.Screen 
+        name="New" 
+        component={New} 
+        options={{
+          tabBarLabel: 'New',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="plus-circle" color={color} size={26} />
+          ),
+        }}  
+        />
+      <Tab.Screen 
+        name="Settings" 
+        component={Settings} 
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="settings-helper" color={color} size={26} />
+          ),
+        }}  
+        />
     </Tab.Navigator>
   </NavigationContainer>
 );
